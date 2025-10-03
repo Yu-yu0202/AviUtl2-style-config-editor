fc978ba7bec03916bb9799e8757db7cad265dd59
Yu-yu0202 <135312165+Yu-yu0202@users.noreply.github.com>
2025-10-03 19:33:09 +0900

    fix: 雑多

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,8 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable}`}
+        style={{ WebkitFontSmoothing: 'antialiased', MozOsxFontSmoothing: 'grayscale' }}
       >
         <LanguageProvider>
           {children}
